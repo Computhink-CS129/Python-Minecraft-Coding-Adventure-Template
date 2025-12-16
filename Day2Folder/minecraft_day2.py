a12c37a10dba178555cdf504aeee813b77666234
Computhink-CS129 <cs129@computhink.com.sg>
2025-12-16 09:04:01 +0000

    Dec 16, 2025, 9:04 AM

--- Day2Folder/minecraft_day2.py
+++ Day2Folder/minecraft_day2.py
@@ -1,2 +1,9 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def chop():
+    for count in range(20):
+        agent.destroy(FORWARD)
+        agent.move(UP, 1)
+    agent.teleport_to_player()
+    
+player.on_chat("chop", chop)
