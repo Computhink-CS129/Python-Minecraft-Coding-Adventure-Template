3cc7fc2bb6e0d6697cb7932a7c608acb0b07e610
Computhink-CS129 <cs129@computhink.com.sg>
2025-12-16 07:13:23 +0000

    Dec 16, 2025, 7:13 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -17,7 +17,7 @@ for letter in name:
         print("Give me a " + letter + "!!")
     
 print("Who is the best!")
-
+print(name +)
 
 
 ########################################################################
