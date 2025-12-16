530b31317c6f2085510cc911b46de0221e83773d
Computhink-CS129 <cs129@computhink.com.sg>
2025-12-16 06:03:01 +0000

    Dec 16, 2025, 6:03 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,39 +1,59 @@
-# Write all your codes for Day 1 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day1")
 
+print("hello from day1 I am Olivia")
+print("this is second line")
 ########################################################################
 # Task 1:
-
+print("I study at Canberra Primary School")
+print("I live at Sembawang Road")
+print("I like eating ramen")
+print("If i had $2,000 as a reward, i would save it and put in my bank")
 
 
 ########################################################################
 # Task 2:
-
-
+var1 = "20"
+var2 = "26"
+ans = var1 + var2
+print(ans)
 
 ########################################################################
 # Task 3:
-
-
+zoo1 = "capybara"
+zoo2 = "turtle"
+zoo3 = "panda"
+print (zoo2)
 
 ########################################################################
 # Task 4:
-
-
-
+var1 = 3
+var2 = 5
+print(var1 + var2)
+print(var1 * var2)
+print(var1 / var2)
 ########################################################################
 # Task 5:
-
-
+name =" Dog"
+food = " bone"
+print("My name is" + name )
 
 ########################################################################
 # Task 6:
+def greetings():
+    print("Hello")
+    print("My name is Olivia")
+    print("I love to swim")
+
+def intro(yourname) :
+    print("Nice to meet you," + yourname) # whatever is ignore
+    print (yourname)
+
+intro ("Nigel")
+intro ("Star")
 
 
-
+greetings() # call a function
 ########################################################################
-# Task 7:
+# Task 7:                                                                                                                                                                                                     111111111111111111111111
 
 
 
